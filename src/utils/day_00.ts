8bcc0aac71fc00e3cf2c7800c6ca4ab1e54fc5ec
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-29 12:11:35 -0600

    more demo solution tricks

--- src/utils/day_00.ts
+++ src/utils/day_00.ts
@@ -8,20 +8,24 @@ const n = 20
 
 // * the invocation is just a joke 🤪
 /// * there's no perl running here... but it'd be a lot cooler if there was!
-export const invocation = '/usr/bin/perl -w ./pascal4lyfe.pl'
+export const invocation = '/usr/bin/perl -w ./pascal_4_lyfe.pl'
 
 // * the entry point for the solution, taking one argument
 // * print is used to "print" a line to the "terminal"
 export async function solution (print: (line: string) => void) {
   for (let i = 1; i <= n; i++) {
+    print(' ') // blank line
+    print('n = ' + i.toString())
     await printPascalsTriangle(print, i)
+    // eslint-disable-next-line promise/param-names
+    await new Promise(r => setTimeout(r, Math.random() * 3 * 1000))
   }
 }
 
 async function printPascalsTriangle (print: (line: string) => void, n: number) {
   let line = ''
 
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i <= n; i++) {
     let C = 1 // used to represent C(line, i)
     for (let j = 1; j <= i; j++) {
       line += String(C) + ' '
