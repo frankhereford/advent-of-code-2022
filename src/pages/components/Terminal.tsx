926a7c097258b59bcdcee43ea2e808427f3d889a
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-29 02:05:21 -0600

    gotta hide that overflow until we scroll

--- src/pages/components/Terminal.tsx
+++ src/pages/components/Terminal.tsx
@@ -77,7 +77,7 @@ export default function Terminal (props: Props) {
   return (
     <>
       {isShown && (
-        <div className="z-[40] absolute top-[5%] right-[5%] w-[70%] mx-auto  drop-shadow-[10px_10px_15px_rgba(0,0,0,0.5)] ">
+        <div className="overflow-hidden z-[40] absolute top-[5%] right-[5%] w-[70%] mx-auto drop-shadow-[10px_10px_15px_rgba(0,0,0,0.5)] ">
           <div className={'w-full shadow-2xl subpixel-antialiased rounded h-[70vh] ' + backgroundColor + ' border-black mx-auto'}>
             <div className="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black" id="headerTerminal">
               <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" id="closebtn" onClick={close}>
