c6c5647397b5b95a93ac6a11b9e9465900d43238
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-28 19:34:20 -0600

    state handling

--- src/pages/components/Terminal.tsx
+++ src/pages/components/Terminal.tsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 interface commandType {
-  command: string;
-  output: string;
+  command: string
+  output: string
 }
 interface Props {
-  commands: commandType[];
-  speed?: number;
+  commands: commandType[]
+  speed?: number
 }
 
-export default function Terminal(props: Props) {
-  const [isShown, setIsShown] = useState(true);
+export default function Terminal (props: Props) {
+  const [isShown, setIsShown] = useState(true)
 
   const [commands, setCommands] = useState<commandType[]>([])
 
@@ -20,20 +20,18 @@ export default function Terminal(props: Props) {
     setCommands(props.commands)
   }, [commands, props.commands])
 
-
-
   function close () {
-    setIsShown(false);
+    setIsShown(false)
   }
 
-  const date = new Date().toLocaleDateString("en-US")
+  const date = new Date().toLocaleDateString('en-US')
   const backgroundColor = 'bg-[#000000bb]'
 
   return (
     <>
       {isShown && (
         <div className="z-[40] absolute top-[5%] right-[5%] w-[70%] mx-auto  drop-shadow-[10px_10px_15px_rgba(0,0,0,0.5)] ">
-          <div className={"w-full shadow-2xl subpixel-antialiased rounded h-[70vh] " + backgroundColor + " border-black mx-auto"}>
+          <div className={'w-full shadow-2xl subpixel-antialiased rounded h-[70vh] ' + backgroundColor + ' border-black mx-auto'}>
             <div className="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black" id="headerTerminal">
               <div className="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3" id="closebtn" onClick={close}>
               </div>
@@ -46,7 +44,7 @@ export default function Terminal(props: Props) {
               </div>
 
             </div>
-            <div className={"pl-1 pt-1 h-auto  text-green-200 font-mono text-xs "} id="console">
+            <div className={'pl-1 pt-1 h-auto  text-green-200 font-mono text-xs '} id="console">
               <p className="pb-1">Last login: {date} on ttys002</p>
               <p className="pb-1">frank@advent-of-code $ echo &quot;$GIT_MSG $GIT_REPOSITORY&quot;</p>
               <p className="pb-1">Fork this on GitHub: <a target='_github' href='https://github.com/frankhereford/advent-of-code-2022'>https://github.com/frankhereford/advent-of-code-2022</a></p>
