5513ebb675a2cc1971fea886119ca4cf1a0ba787
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-29 03:11:40 -0600

    tune the snow

--- src/pages/components/Snowfall.tsx
+++ src/pages/components/Snowfall.tsx
@@ -10,7 +10,7 @@ export default function SnowfallComponent (props: Props) {
       {props.plane === 'distant' && (
         <Snowfall
           color={'#E3E3E3'}
-          speed={[0.5, 0.7]}
+          speed={[0.2, 0.4]}
           snowflakeCount={750}
           radius={[2, 4]}
           wind={[-0.2, 0.2]}
@@ -23,7 +23,7 @@ export default function SnowfallComponent (props: Props) {
       {props.plane === 'midground' && (
         <Snowfall
           color={'#E3E3E3'}
-          speed={[1.5, 1.7]}
+          speed={[0.4, 0.9]}
           snowflakeCount={500}
           radius={[1.5, 3.0]}
           wind={[-0.5, 0.5]}
@@ -36,10 +36,11 @@ export default function SnowfallComponent (props: Props) {
       {props.plane === 'foreground' && (
         <Snowfall
           color={'#E3E3E3'}
-          speed={[0.5, 0.7]}
+          speed={[1.2, 1.5]}
           snowflakeCount={50}
           radius={[0.5, 1.5]}
-          wind={[-0.5, 0.5]}
+          changeFrequency={1000}
+          wind={[-1.5, 1.5]}
           style={{
             filter: 'blur(1.0px)',
             zIndex: 50
