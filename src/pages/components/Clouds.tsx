b40edeb122e13fde072d901f7dd9fcabc43755ca
Frank Hereford <frankhereford@users.noreply.github.com>
2022-11-30 19:19:23 -0600

    rework offset generation

--- src/pages/components/Clouds.tsx
+++ src/pages/components/Clouds.tsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react'
 import { useInterval } from 'usehooks-ts'
 
-export default function Clouds (props: { opacity?: number, windChangeDelay?: number }) {
+// * use this to get a random number but according to the normal distribution
+function randomBoxMillerTransform (): number {
+  let u = 0
+  let v = 0
+  while (u === 0) u = Math.random() // Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random()
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
+  num = num / 10.0 + 0.5 // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randomBoxMillerTransform() // resample between 0 and 1
+  return num
+}
+
+export default function Clouds (props: { opacity?: number, windChangeDelay?: number, windChangeVariability?: number }) {
   const [windChangeDelay] = useState((props.windChangeDelay ?? 20) * 1000)
   const [transitionDuration] = useState(props.windChangeDelay ?? 20)
   const [cloudXLocation, setCloudXLocation] = useState(0)
   const imageWidth = 1440
 
   function computeNewXLocation () {
-    setCloudXLocation(Math.floor(Math.random() * imageWidth * 2) - imageWidth)
+    const newXLocation = randomBoxMillerTransform() * imageWidth - (imageWidth / 2) * (props.windChangeVariability ?? 1)
+    setCloudXLocation(newXLocation)
   }
 
   // ? how do you do this correctly, there must be a better way
