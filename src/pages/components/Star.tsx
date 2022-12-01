0e2581731250d034fcb7810254b83454ead2effc
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-01 01:22:54 -0600

    tweak the look of the stars

--- src/pages/components/Star.tsx
+++ src/pages/components/Star.tsx
@@ -5,9 +5,9 @@ export default function Star (props: { day: string, isHover: boolean }) {
   const setDay = useContext(DayPicker)
   return (
     <>
-      <div onClick={() => { setDay(parseInt(props.day)) }} className="relative w-[6rem] h-[5rem] drop-shadow-2xl">
+      <div onClick={() => { setDay(parseInt(props.day)) }} className="relative w-[6rem] h-[5rem] drop-shadow-xl">
         <div className='absolute top-0 left-0 text-[4rem]'>⭐️</div>
-        { props.isHover && <div className='absolute w-[4rem] text-slate-700 top-[1.9rem] text-center text-[1.7rem]'>{props.day}</div> }
+        { props.isHover && <div className='absolute w-[4rem] text-slate-500 top-[2.00rem] text-center text-[1.5rem]'>{props.day}</div> }
       </div>
     </>
   )
