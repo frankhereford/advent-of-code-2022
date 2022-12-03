8919c542b1cdeed5e999113eead055e1f480e5e8
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-02 23:45:35 -0600

    part 1

--- src/utils/day_03.ts
+++ src/utils/day_03.ts
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable padded-blocks */
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { input, testInput } from './day_03_input'
 
 const terminalSpeed = 1
 const terminalVariability = 1
 
-const problemStatement = 'Day 3 task: Introduce the tragedy..'
-const invocation = '️/legacy/bin/shakespeare fairVerona.script'
+const problemStatement = 'Day 3 task: The packs are not packed right! Help the elves get organized.'
+const invocation = '️/bin/lua packPacks.lua'
+const mod = 20
 
 async function solution (print: (line?: string | null) => void) {
   print() // blank line
@@ -18,28 +23,46 @@ export default day
 
 // * 👇 Functions and/or whatever is helpful to get the actual job done down here 👇
 
-function puzzleFunction (input: string, print: (line?: string) => void) {
-  const intro = [
-    'Two households, both alike in dignity,',
-    'In fair Verona, where we lay our scene,',
-    'From ancient grudge break to new mutiny,',
-    'Where civil blood makes civil hands unclean.',
-    'From forth the fatal loins of these two foes',
-    'A pair of star-cross\'d lovers take their life;',
-    'Whose misadventured piteous overthrows',
-    'Do with their death bury their parents\' strife.',
-    'The fearful passage of their death-mark\'d love,',
-    'And the continuance of their parents\' rage,',
-    'Which, but their children\'s end, nought could remove,',
-    'Is now the two hours\' traffic of our stage;',
-    'The which if you with patient ears attend,',
-    'What here shall miss, our toil shall strive to mend.'
-  ]
-  intro.map((line) => {
-    print(line + '\n')
-    return true
-  })
+interface Letter {
+  [key: string]: number
+}
 
+function scoreLetter (letterArg: string) {
+  const scores: Letter = {}
+  for (let i = 1; i <= 26; i++) {
+    const letter = String.fromCharCode(i + 96)
+    scores[letter] = i
+  }
+  for (let i = 0; i < 26; i++) {
+    const letter = String.fromCharCode(i + 65)
+    scores[letter] = i + 1 + 26
+  }
+  return scores[letterArg]
+}
+
+function puzzleFunction (input: string, print: (line?: string) => void) {
+  const lines = input.split('\n')
+  const scores = lines.map((line, index) => {
+    const leftString = line.slice(0, line.length / 2)
+    const rightString = line.slice(line.length / 2, line.length)
+    const left = new Set(leftString.split(''))
+    const right = new Set(rightString.split(''))
+    const intersection = [...left].filter(x => right.has(x))
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const score = scoreLetter(intersection[0]!)
+    if ((index % mod) === 0) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      print(`Pack #${index}: I shook it real hard, and I got out two ${intersection[0]!} letters worth ${score!}.\n`)
+    }
+    return score
+  })
+  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+  const sum = scores.reduce((a, b) => {
+    if (a == null || b == null) return 0
+    return a + b
+  }, 0)
+  if (sum == null) return
+  print(`⭐️ The backpack priority initial scan reads: ${sum}`)
   // * return null here to get that extra space before the waiting terminal prompt
   return null
 }
