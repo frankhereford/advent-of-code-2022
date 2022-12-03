d974f519eb2941352571b633d2bbe269659e5bd8
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-02 21:22:27 -0600

    smooth over the number edge

--- src/pages/components/Star.tsx
+++ src/pages/components/Star.tsx
@@ -7,7 +7,7 @@ export default function Star (props: { day: string, isHover: boolean }) {
     <>
       <div onClick={() => { setDay(parseInt(props.day)) }} className="relative w-[6rem] h-[5rem] drop-shadow-xl">
         <div className='absolute top-0 left-0 text-[4rem]'>⭐️</div>
-        { props.isHover && <div className='absolute w-[4rem] text-slate-500 top-[2.00rem] text-center text-[1.5rem]'>{props.day}</div> }
+        <div className={ (props.isHover ? 'opacity-100' : 'opacity-0') + ' absolute w-[4rem] text-slate-500 top-[2.00rem] text-center text-[1.5rem] transition-opacity duration-1000'}>{props.day}</div>
       </div>
     </>
   )
