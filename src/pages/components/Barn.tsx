59d49ba00d5a58c6433ed32573ca6dc11f625c1d
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-02 22:16:44 -0600

    fix next Image warning

--- src/pages/components/Barn.tsx
+++ src/pages/components/Barn.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
 
 export default function Barn () {
   return (
     <>
       <div className='z-[30] absolute bottom-0 overflow-x-hidden w-screen'>
-        <Image priority={true} className='w-[3292px] barn' src="/snowscape.webp" alt="red barn by a lake" width='3202' height="711" />
+        <img className='w-[3292px] h-auto barn' src="/snowscape.webp" alt="red barn by a lake" width='3292' height="711" />
       </div>
     </>
   )
