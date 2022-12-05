af85ca3d2b5e5eaca6952df2f35e07e7e1ac9c10
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-05 00:18:15 -0600

    prediction

--- src/utils/day_05.ts
+++ src/utils/day_05.ts
@@ -43,6 +43,7 @@ function puzzleFunction (input: string, print: (line?: string) => void) {
   return null
 }
 
+// pre part 1 solve, i bet the twist is they get a crane upgrade and can move multiple at once
 
 
 function processInstructions (instructions: Move[], stacks: string[][], print: (line?: string) => void) {
