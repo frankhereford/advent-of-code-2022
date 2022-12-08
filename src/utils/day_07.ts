c1334084ec659c131e3ee6f5c78490410ab402ee
Frank Hereford <frankhereford@users.noreply.github.com>
2022-12-07 23:14:14 -0600

    record this in git

--- src/utils/day_07.ts
+++ src/utils/day_07.ts
@@ -2,18 +2,18 @@
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable padded-blocks */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { fs } from 'memfs'
 import { input, testInput } from './day_07_input'
 
 const terminalSpeed = 1
 const terminalVariability = 1
 
-const problemStatement = 'Day 7 task: You know, nothing big, just reinvent `ncdu`, for Christmas\'s sake.'
-const invocation = '️/bin/sh rm.sh'
+const problemStatement = 'Day 7 task: You know, nothing big, just reinvent `ncdu`, for Christmas\' sake.'
+const invocation = '️du -Aachk / # Gesundheit'
 
 async function solution (print: (line?: string | null) => void) {
   print() // blank line
-  print(puzzleFunction(input, print))
+  print(puzzleFunction(testInput, print))
   print('frank@advent-of-code $')
 }
 
@@ -24,10 +24,47 @@ export default day
 
 // ! Prediction before laying in any code: The twist is to execute the fewest instructions possible to achieve n% disk capacity.
 
-function puzzleFunction (input: string, print: (line?: string) => void) {
 
+function puzzleFunction(input: string, print: (line?: string) => void) {
+  const lines = input.split('\n')
+}
+
+/*
+function puzzleFunction (input: string, print: (line?: string) => void) {
+  const lines = input.split('\n')
+
+  const fileSystem = {}
+  const pwd = ['/'] // we start at the root of the file system
+
+  let currentOperation: string | null = null
+  lines.map((line, index) => {
+    console.log('')
+    console.log('line: ', line)
+
+    const inputPattern = /^(\${0,1})\s*(\S+)\s*(\S*)\s*(\S*).*/ // Why do regexes have to look like line noise?
+    const result = line.match(inputPattern)
+    if (result == null) return false
+
+    if (result[1] === '$') {
+      // command
+      console.log('incoming command', result[2])
+      currentOperation = null
+      if (result[2] === 'cd') {
+        console.log('📂')
+      } else if (result[2] === 'ls') {
+        console.log('📋')
+        currentOperation = 'ls'
+        // list
+      }
+    } else {
+      console.log(`Current operation: ${currentOperation ?? ''}`)
+      // result
+    }
+    return true
+  })
 
 
   // * return null here to get that extra space before the waiting terminal prompt
   return null
 }
+*/
